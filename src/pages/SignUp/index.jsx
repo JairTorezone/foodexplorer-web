6129f554041f817e287ff418e1d44140c51db33b
Jair Torezone <jairtorezone@gmail.com>
2024-05-21 21:07:38 -0300

    project clean

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.js";
+
+function SignUp() {
+  return (
+    <>
+      <h1>Hello word - SignUp</h1>
+    </>
+  );
+}
+
+export default SignUp;
---
+++ src/styles/theme.js
@@ -0,0 +1,23 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#262529",
+    BACKGROUND_800: "#312E38",
+    BACKGROUND_700: "#272024",
+    BACKGROUND_600: "#0D0C0F",
+    BACKGROUND_500: "#1C1B1E;",
+
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+
+    GRAY_100: "#E5E5E5",
+    GRAY_200: "#999591",
+    GRAY_300: "#312e38",
+    GRAY_400: "#3E3B47",
+    GRAY_500: "#948F99",
+    GRAY_600: "#262529",
+    GRAY_700: "#CAC4CF",
+    GRAY_800: "#999591",
+  },
+};
+
+//  color: ${({ theme }) => theme.COLORS.WHITE};
