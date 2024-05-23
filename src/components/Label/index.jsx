36bb5aa9f084986c563de671bfe59c5a52239c65
Jair Torezone <jairtorezone@gmail.com>
2024-05-22 21:29:21 -0300

    Label component created

---
+++ src/components/Label/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function Label({ title, ...rest }) {
+  return <Container {...rest}>{title}</Container>;
+}
---
+++ src/components/Label/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.label`
+  color: ${({ theme }) => theme.COLORS.GRAY_400};
+  font-family: 16px;
+`;
