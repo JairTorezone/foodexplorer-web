6a1786bea45065250060b3a97c11b3e861204638
Jair Torezone <jairtorezone@gmail.com>
2024-05-22 21:30:01 -0300

    add images and SignUp page created

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,40 @@
 import "./styles.js";
-import { Container } from "./styles.js";
+import logoImg from "../../assets/logo.svg";
+import { Input } from "../../components/Input/index.jsx";
+import { Container, Banner, Form, Wrapper } from "./styles.js";
+import { Label } from "../../components/Label/index.jsx";
+import { Button } from "../../components/Button/index.jsx";
 
 function SignUp() {
   return (
     <>
       <Container>
-        <h1>Hello word - SignUp</h1>
+        <Banner>
+          <img src={logoImg} alt="logo" />
+          <h1>food explorer</h1>
+        </Banner>
+
+        <Form>
+          <h2>Crie sua conta</h2>
+          <Wrapper>
+            <Label title="Seu nome" />
+            <Input placeholder="Exemplo: Maria da Silva" />
+          </Wrapper>
+
+          <Wrapper>
+            <Label title="E-mail" />
+            <Input placeholder="Exemplo: exemplo@exemplo.com.br" />
+          </Wrapper>
+
+          <Wrapper>
+            <Label title="Senha" />
+            <Input placeholder="No mínimo 6 caracteres" />
+          </Wrapper>
+
+          <Button title="Criar conta" />
+        </Form>
+
+        <a href="/">Já tenho uma conta2</a>
       </Container>
     </>
   );
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,5 +1,62 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: red;
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  padding: 0px 10px;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_1000};
+
+  > a {
+    display: block;
+    text-align: center;
+    margin-top: 32px;
+    text-decoration: nome;
+
+    font-family: Poppins;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px;
+
+    /* border: 1px solid red; */
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+`;
+
+export const Banner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  margin-bottom: 73px;
+
+  > h1 {
+    font-size: 35px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
+export const Form = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 250px;
+
+  > h2 {
+    display: none;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  margin-bottom: 25px;
 `;
